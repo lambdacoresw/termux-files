f90eb475ebc5650670dbc207e0ce1672235fd223
Ismail YESILYURT <lambdacoresw@gmail.com>
2026-05-31 22:47:51 +0300

    fix antigravity setup url for termux

--- agcli.setup.sh
+++ agcli.setup.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-curl -fsSL https://antigravity.google/cli/install.sh | bash
+#curl -fsSL https://antigravity.google/cli/install.sh | bash
+
+# for termux 
+curl -fsSL https://raw.githubusercontent.com/wallentx/antigravity-cli-termux/dev/install.sh | bash
