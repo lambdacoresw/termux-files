6ccc81dbdea2c9a95fb331b267d1ec022c1a7b3f
freeman <lambdacoresw@gmail.com>
2026-06-02 00:50:53 +0300

    fix gradle version issues

--- android.sdk/androidstudiotemplates/EmptyActivityKotlinJetpack/app/build.gradle.kts
+++ android.sdk/androidstudiotemplates/EmptyActivityKotlinJetpack/app/build.gradle.kts
@@ -5,11 +5,7 @@ plugins {
 
 android {
     namespace = "org.lambdacoresw.emptyactivitykotlinjetpack"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "org.lambdacoresw.emptyactivitykotlinjetpack"
